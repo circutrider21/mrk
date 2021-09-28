b371ec77bab72ef4753f379aacf2d3b75cf77a03
Yusuf M <circutrider21@gmail.com>
2021-09-28 13:00:35 -0500

    kernel: cleanup for rust

--- kernel/include/lib/fmt.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIB_FMT_H
-#define LIB_FMT_H
-
-#include <stdarg.h>
-#include <stddef.h>
-
-int vsnprintf(char *buf, size_t size, const char *format, va_list args);
-
-#endif // LIB_FMT_H
--- tools/clang-format.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# A Simple format to run clang-format over every file in project directory
-# WARNING: Only run this in the root directory of the project
-
-find . -iname *.h -o -iname *.cc | xargs clang-format -i
-
