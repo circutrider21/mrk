94ef66b6c23d947994f82b6567011ae1b00b0169
Yusuf M <circutrider21@gmail.com>
2021-09-29 11:26:15 -0500

    kernel: Created skeleton

---
+++ kernel/src/main.rs
@@ -0,0 +1,36 @@
+#![no_std]  // Disable the rust standard library
+#![no_main] // 'fn main' isn't our entry point, so disable its warning here
+
+use core::panic::PanicInfo;
+use stivale_boot::v2::*;
+
+#[repr(C, align(4096))]
+struct KStack<T>(T);
+
+/// The kernel stack (16 KiB)
+static STACK: KStack<[u8; 4096 * 4]> = KStack([0; 4096 * 4]);
+
+/// Request a framebuffer from the bootloader
+static FRAMEBUFFER: StivaleFramebufferHeaderTag = StivaleFramebufferHeaderTag::new()
+    .framebuffer_bpp(24);
+
+/// The stivale2 header
+#[link_section = ".stivale2hdr"]
+#[no_mangle]
+#[used]
+static STIVALE_HDR: StivaleHeader = StivaleHeader::new()
+    .stack(&STACK.0[4096 * 4 - 4096] as *const u8)
+    .tags((&FRAMEBUFFER as *const StivaleFramebufferHeaderTag).cast());
+
+/// Kernel entry point, the bootloader jumps here.
+#[no_mangle]
+pub extern "C" fn mrk_entry(bootinfo: &'static StivaleStruct) -> ! {
+    let bname: &str = bootinfo.bootloader_brand();
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
