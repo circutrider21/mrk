f68faf4712cf5718b85617cce6cfbf486b0284d1
Yusuf M <circutrider21@gmail.com>
2021-09-29 20:22:30 -0500

    kernel: Added logging facilities

---
+++ kernel/src/arch/x86_64/mod.rs
@@ -0,0 +1,24 @@
+/// Functions to access x86 ports
+#[inline]
+pub unsafe fn outb(port: u16, value: u8) {
+    asm!(
+       "out dx, al",
+       in("dx") port,
+       in("al") value,
+       options(preserves_flags, nomem, nostack)
+    );
+}
+
+#[inline]
+pub unsafe fn inb(port: u16) -> u8 {
+    let ret: u8;
+
+    asm!(
+        "in al, dx",
+        in("dx") port,
+        out("al") ret,
+        options(preserves_flags, nomem, nostack)
+    );
+
+    ret
+}
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,8 +1,14 @@
 #![no_std]  // Disable the rust standard library
 #![no_main] // 'fn main' isn't our entry point, so disable its warning here
+#![feature(
+    asm,
+    thread_local,
+    global_asm,
+)]
 
 use core::panic::PanicInfo;
 use stivale_boot::v2::*;
+use log::info;
 
 #[repr(C, align(4096))]
 struct KStack<T>(T);
@@ -22,10 +28,19 @@ static STIVALE_HDR: StivaleHeader = StivaleHeader::new()
     .stack(&STACK.0[4096 * 4 - 4096] as *const u8)
     .tags((&FRAMEBUFFER as *const StivaleFramebufferHeaderTag).cast());
 
+// Import the logger impl
+mod logger;
+
 /// Kernel entry point, the bootloader jumps here.
 #[no_mangle]
 pub extern "C" fn mrk_entry(bootinfo: &'static StivaleStruct) -> ! {
     let bname: &str = bootinfo.bootloader_brand();
+
+    // Setup the logger
+    logger::init();
+    info!("Hello, World!\n");
+    info!("Bootloader Brand -> {}\n", bname);
+    
     loop {}
 }
 
